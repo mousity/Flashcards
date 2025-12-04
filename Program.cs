d75edcf1864d01ac013e8f53ce2abb819733abfa
Mousity <samr.mouna@gmail.com>
2025-12-03 23:29:20 -0500

    Added constructor for data controller

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿class Program()
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+class Program()
 {
     static void Main(string[] args)
     {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
 
+        string? connectionString = config.GetConnectionString("Flashcards");
+
+        Console.WriteLine(connectionString);
+        Console.ReadLine();
     }
 }
