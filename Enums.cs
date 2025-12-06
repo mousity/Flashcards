fc44188afef57e74ddd6c3878dcfedc441e3993b
Mousity <samr.mouna@gmail.com>
2025-12-05 22:41:56 -0500

    Added short menu

---
+++ Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flashcards
+{
+    internal class Enums
+    {
+        internal enum MenuOptions
+        {
+            ViewAllStacks,
+            Quit
+        }
+    }
+}
--- UserInterface.cs
+++ UserInterface.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spectre.Console;
+using static flashcards.Enums;
 
 namespace flashcards
 {
@@ -10,6 +12,19 @@ namespace flashcards
     {
         public static void MainMenu()
         {
+            bool runMenu = true;
+            while (runMenu)
+            {
+                AnsiConsole.Markup("[green]Welcome to your Flashcards program![/]");
+
+                var choice = AnsiConsole.Prompt(
+                        new SelectionPrompt<MenuOptions>()
+                        .Title("\nChoose an option from the menu below to continue.\n")
+                        .AddChoices(MenuOptions.ViewAllStacks,
+                                    MenuOptions.Quit)
+                    );
+            }
+
 
         }
     }
